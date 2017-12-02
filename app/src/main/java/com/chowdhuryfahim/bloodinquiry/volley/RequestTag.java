c7382c1e7162abfac7319215e48d3edf870bcf5e
Misbah Ahmad Chowdhury <imiiucian@gmail.com>
2017-12-02 21:10:44 +0600

    Update RequestTag.java

--- app/src/main/java/com/chowdhuryfahim/bloodinquiry/volley/RequestTag.java
+++ app/src/main/java/com/chowdhuryfahim/bloodinquiry/volley/RequestTag.java
@@ -6,19 +6,6 @@ package com.chowdhuryfahim.bloodinquiry.volley;
  */
 public class RequestTag {
 
-    public static final String BASE_URL = "http://www.minhajulalam.com/misbah/bi/";
-    public static final String LOGIN_URL = BASE_URL+"login.php";
-    public static final String ORG_REG_URL = BASE_URL+"register_org.php";
-    public static final String DONOR_REG_URL = BASE_URL+"register_donor.php";
-    public static final String GET_ORG_URL = BASE_URL+"get_org.php";
-    public static final String ORG_LOGIN_URL = BASE_URL+"org_login.php";
-    public static final String DONOR_PROFILE_UPDATE_URL = BASE_URL+"donor_update.php";
-    public static final String DONOR_PROFILE_DELETE_URL = BASE_URL+"delete_donor.php";
-    public static final String GET_DONOR_URL = BASE_URL+"get_donors.php";
-    public static final String  ORG_PROFILE_UPDATE_URL= BASE_URL+"update_org.php";
-    public static final String DOWNLOAD_URL = BASE_URL+"generate_pdf.php?username=";
-    public static final String EMAIL_SEND_URL = BASE_URL+"mail_helper/mail_sender.php";
-    public static final String RESET_PASSWORD_URL = BASE_URL + "reset_password.php";
 
 
 }
